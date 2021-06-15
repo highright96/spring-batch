c3a4f795a45c5d4c60a291c8ed59eeedc3a2d12f
highright96 <nsw5296@naver.com>
2021-06-16 02:45:57 +0900

    [Add] 프로젝트 세팅, MySQL 연결

---
+++ src/test/java/dev/highright96/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.highright96.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
