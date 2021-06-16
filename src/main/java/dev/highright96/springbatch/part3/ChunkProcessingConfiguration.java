c74b1b8915725178e49784e07f182d62454da3de
highright96 <nsw5296@naver.com>
2021-06-16 17:45:53 +0900

    [Add] Task, Chunk 기반 배치, JobParameters, @JobScope @StepScope 예제

---
+++ src/main/java/dev/highright96/springbatch/part3/ChunkProcessingConfiguration.java
@@ -0,0 +1,115 @@
+package dev.highright96.springbatch.part3;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.micrometer.core.instrument.util.StringUtils.isNotEmpty;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ChunkProcessingConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job chunkProcessingJob() {
+        return jobBuilderFactory.get("chunkProcessingJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.taskBaseStep())
+                .next(this.chunkBaseStep(null)) // null로 파라미터를 넘겨도 스프링이 자동으로 감지해 알맞는 chunkSize를 넣어준다.
+                .build();
+    }
+
+    @Bean
+    public Step taskBaseStep() {
+        return stepBuilderFactory.get("taskBaseStep")
+                .tasklet(this.tasklet(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step chunkBaseStep(@Value("#{jobParameters[chunkSize]}") String chunkSize) {
+        return stepBuilderFactory.get("chunkBaseStep")
+                .<String, String>chunk(isNotEmpty(chunkSize) ? Integer.parseInt(chunkSize) : 10)// input, output , 10개로 쪼개서 진행
+                .reader(itemReader())// input -> input
+                .processor(itemProcessor()) // input -> output
+                .writer(itemWriter()) // List<Input>
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet tasklet(@Value("#{jobParameters[chunkSize]}") String value) {
+        List<String> items = getItems();
+
+        return ((contribution, chunkContext) -> {
+            StepExecution stepExecution = contribution.getStepExecution();
+
+            //JobParameters jobParameters = stepExecution.getJobParameters();
+            //String value = jobParameters.getString("chunkSize", "10");
+
+            int chunkSize = isNotEmpty(value) ? Integer.parseInt(value) : 10;
+            int fromIndex = stepExecution.getReadCount();
+            int toIndex = fromIndex + chunkSize;
+
+            if (fromIndex >= items.size()) {
+                return RepeatStatus.FINISHED;
+            }
+
+            List<String> subList = items.subList(fromIndex, toIndex);
+
+            log.info("task item size : {}", subList.size());
+
+            stepExecution.setReadCount(toIndex);
+
+            return RepeatStatus.CONTINUABLE;
+        });
+    }
+
+    private ItemReader<String> itemReader() {
+        return new ListItemReader<>(getItems());
+    }
+
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", Spring Batch";
+    }
+
+    private ItemWriter<String> itemWriter() {
+        return items -> log.info("chunk item size : {}", items.size());
+        //return items -> items.forEach(log::info);
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            items.add(i + "Hello");
+        }
+        return items;
+    }
+}
