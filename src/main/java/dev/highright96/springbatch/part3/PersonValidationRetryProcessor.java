cb790e01158667c702046b2bfe6f9d7baefab75a
highright96 <nsw5296@naver.com>
2021-06-21 23:53:41 +0900

    [Add] RetryListener 예제

--- src/main/java/dev/highright96/springbatch/part3/PersonValidationRetryProcessor.java
+++ src/main/java/dev/highright96/springbatch/part3/PersonValidationRetryProcessor.java
@@ -1,9 +1,14 @@
 package dev.highright96.springbatch.part3;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.RetryListener;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.retry.support.RetryTemplateBuilder;
 
+@Slf4j
 public class PersonValidationRetryProcessor implements ItemProcessor<Person, Person> {
 
     private final RetryTemplate retryTemplate;
@@ -12,6 +17,7 @@ public class PersonValidationRetryProcessor implements ItemProcessor<Person, Per
         this.retryTemplate = new RetryTemplateBuilder()
             .maxAttempts(3)
             .retryOn(NotFoundNameException.class)
+            .withListener(new SavePersonRetryListener())
             .build();
     }
 
@@ -31,5 +37,27 @@ public class PersonValidationRetryProcessor implements ItemProcessor<Person, Per
             //RecoveryCallBack
             return item.unknownName();
         });
+    }
+
+    // RetryTemplate 전처리, 후처리, 에러처리를 담당하는 리스너
+    public static class SavePersonRetryListener implements RetryListener {
+
+        @Override
+        public <T, E extends Throwable> boolean open(RetryContext context,
+            RetryCallback<T, E> callback) {
+            return true;
+        }
+
+        @Override
+        public <T, E extends Throwable> void close(RetryContext context,
+            RetryCallback<T, E> callback, Throwable throwable) {
+            log.info("close");
+        }
+
+        @Override
+        public <T, E extends Throwable> void onError(RetryContext context,
+            RetryCallback<T, E> callback, Throwable throwable) {
+            log.info("onError");
+        }
     }
 }
